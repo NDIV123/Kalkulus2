30553f21ddb307926a9d26628cb40967c9af6c76
baktidwipamungkas <baktidwipamungkas@gmail.com>
2024-05-09 22:22:27 +0700

    membuat arrow slider bagian 1

--- app/(tampilan)/Materi.jsx
+++ app/(tampilan)/Materi.jsx
@@ -1,31 +1,30 @@
 const MateriPage = () => {
     return (
         <>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"></link>
+        <body>
             <div className="section">
-                <div className="vektorperkalian">
-                    <div className="card">
-                        <h1 className="judul">VEKTOR PERKALIAN</h1>
-                        <p className="pmateri1">Perkalian vektor adalah operasi matematika yang menghasilkan vektor baru dari dua atau lebih vektor. Ada dua jenis perkalian vektor yang umum: perkalian dot (skalar) dan perkalian cross (vektor).</p>
-                        <p>Rumus: A⋅B=∣A∣∣B∣cos(θ)</p>
-                    </div>
-                </div>
-
-                <div className="sudut2vektor">
-                    <div className="card">
-                        <h1>SUDUT ANTARA DUA VEKTOR</h1>
-                        <p>Sudut antara dua vektor adalah sudut yang terbentuk antara kedua vektor tersebut ketika ditempatkan pada titik asal yang sama. Sudut ini dapat dihitung menggunakan rumus trigonometri.</p>
-                        <p>Rumus: cos(θ)= A⋅B / ​∣A∣∣B∣</p>
-                    </div>
-                </div>
-
-                <div className="cosinusarah">
-                    <div className="card">
-                        <h1>COSINUS ARAH</h1>
-                        <p>Cosinus arah adalah nilai kosinus dari sudut antara dua vektor. Ini menggambarkan seberapa sejajar atau seberlawanan kedua vektor tersebut.</p>
-                        <p>Rumus: Vektor OP = √(〖a1〗^2+ 〖a2〗^2+ 〖a3〗^2 ) </p>
-                    </div>
-                </div>
-            </div>
+                <i class="fa-solid fa-angle-left"></i>
+                    <ul className="ul-list">
+                        <li className="card">
+                            <h1 className="judul">VEKTOR PERKALIAN</h1>
+                            <p>Perkalian vektor adalah operasi matematika yang menghasilkan vektor baru dari dua atau lebih vektor. Ada dua jenis perkalian vektor yang umum: perkalian dot (skalar) dan perkalian cross (vektor).</p>
+                            <p>Rumus: A⋅B=∣A∣∣B∣cos(θ)</p>
+                        </li>
+                        <li className="card">
+                            <h1 className="judul">SUDUT ANTARA DUA VEKTOR</h1>
+                            <p>Sudut antara dua vektor adalah sudut yang terbentuk antara kedua vektor tersebut ketika ditempatkan pada titik asal yang sama. Sudut ini dapat dihitung menggunakan rumus trigonometri.</p>
+                            <p>Rumus: cos(θ)= A⋅B / ​∣A∣∣B∣</p>
+                        </li>
+                        <li className="card">
+                            <h1 className="judul">COSINUS ARAH</h1>
+                            <p>Cosinus arah adalah nilai kosinus dari sudut antara dua vektor. Ini menggambarkan seberapa sejajar atau seberlawanan kedua vektor tersebut.</p>
+                            <p>Rumus: Vektor OP = √(〖a1〗^2+ 〖a2〗^2+ 〖a3〗^2 ) </p>
+                        </li>
+                    </ul>
+                    <i class="fa-solid fa-angle-right"></i>
+            </div>  
+        </body>
         </>
     )
 }
