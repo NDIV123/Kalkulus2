b9f91501276a2f30aec20517136a18f9771d4fcf
baktidwipamungkas <baktidwipamungkas@gmail.com>
2024-05-10 09:57:36 +0700

    membuat slide card bagian 2

--- app/(tampilan)/Materi.jsx
+++ app/(tampilan)/Materi.jsx
@@ -2,29 +2,33 @@ const MateriPage = () => {
     return (
         <>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"></link>
-        <body>
             <div className="section">
                 <i class="fa-solid fa-angle-left"></i>
-                    <ul className="ul-list">
-                        <li className="card">
+                    <ul className="ul-listMateri">
+                        <li className="cardMateri">
                             <h1 className="judul">VEKTOR PERKALIAN</h1>
+                            <span className="paragrafmateri">
                             <p>Perkalian vektor adalah operasi matematika yang menghasilkan vektor baru dari dua atau lebih vektor. Ada dua jenis perkalian vektor yang umum: perkalian dot (skalar) dan perkalian cross (vektor).</p>
                             <p>Rumus: A⋅B=∣A∣∣B∣cos(θ)</p>
+                            </span>
                         </li>
-                        <li className="card">
+                        <li className="cardMateri">
                             <h1 className="judul">SUDUT ANTARA DUA VEKTOR</h1>
+                            <span className="paragrafmateri">
                             <p>Sudut antara dua vektor adalah sudut yang terbentuk antara kedua vektor tersebut ketika ditempatkan pada titik asal yang sama. Sudut ini dapat dihitung menggunakan rumus trigonometri.</p>
                             <p>Rumus: cos(θ)= A⋅B / ​∣A∣∣B∣</p>
+                            </span>
                         </li>
-                        <li className="card">
+                        <li className="cardMateri">
                             <h1 className="judul">COSINUS ARAH</h1>
+                            <span className="paragrafmateri">
                             <p>Cosinus arah adalah nilai kosinus dari sudut antara dua vektor. Ini menggambarkan seberapa sejajar atau seberlawanan kedua vektor tersebut.</p>
                             <p>Rumus: Vektor OP = √(〖a1〗^2+ 〖a2〗^2+ 〖a3〗^2 ) </p>
+                            </span>
                         </li>
                     </ul>
                     <i class="fa-solid fa-angle-right"></i>
             </div>  
-        </body>
         </>
     )
 }
