c0d79980754cddfc8e5a2b77b795b36b7a53bf79
NDIV123 <dmarshelano@gmail.com>
2024-05-12 18:59:36 +0700

    edit hitung page

--- app/components/HasilHitung.jsx
+++ app/components/HasilHitung.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   angleBetweenVectors,
   dotProduct,
@@ -9,106 +9,107 @@ import {
   kosinusC,
 } from "../libs/function";
 
-const HasilHitung = (prop) => {
-  const { vektor1, vektor2 } = prop;
+const HasilHitung = ({ vektor1, vektor2 }) => {
   const [hasil, setHasil] = useState(null);
 
-  
+  useEffect(() => {
+    vektor1 && handleDisplay();
+    console.log(hasil);
+  }, [vektor1]);
+
   const handleDisplay = () => {
     vektor1?.length === 3
-    ? setHasil({
-      sudutVektor: angleBetweenVectors(vektor1, vektor2),
-      perkalianVektor: dotProduct(vektor1, vektor2),
-      cosinusArah: {
-        vektor1: {
-          cosa: kosinusA(vektor1),
-          cosb: kosinusB(vektor1),
-          cosc: kosinusC(vektor1),
-        },
-        vektor2: {
-          cosa: kosinusA(vektor2),
-          cosb: kosinusB(vektor2),
-          cosc: kosinusC(vektor2),
-        },
-      },
-    })
-    : setHasil({
-      sudutVektor: angleBetweenVectors(vektor1, vektor2),
-      perkalianVektor: dotProduct(vektor1, vektor2),
-      cosinusArah: {
-        vektor1: {
-          cosa: kosinusA(vektor1),
-          cosb: kosinusB(vektor1),
-        },
-        vektor2: {
-          cosa: kosinusA(vektor2),
+      ? setHasil({
+          sudutVektor: angleBetweenVectors(vektor1, vektor2),
+          perkalianVektor: dotProduct(vektor1, vektor2),
+          cosinusArah: {
+            vektor1: {
+              cosa: kosinusA(vektor1),
+              cosb: kosinusB(vektor1),
+              cosc: kosinusC(vektor1),
+            },
+            vektor2: {
+              cosa: kosinusA(vektor2),
+              cosb: kosinusB(vektor2),
+              cosc: kosinusC(vektor2),
+            },
+          },
+        })
+      : setHasil({
+          sudutVektor: angleBetweenVectors(vektor1, vektor2),
+          perkalianVektor: dotProduct(vektor1, vektor2),
+          cosinusArah: {
+            vektor1: {
+              cosa: kosinusA(vektor1),
+              cosb: kosinusB(vektor1),
+            },
+            vektor2: {
+              cosa: kosinusA(vektor2),
               cosb: kosinusB(vektor2),
             },
           },
         });
-      };
-      
-      vektor1 ? handleDisplay : null;
+  };
 
-      const handleCheck = () => {
-        console.log(prop);
-      };
-      
-      return (
-        <div className="w-full h-full p-4 ">
-      <button className="btn btn-primary" onClick={handleCheck}>
-        Pencet
-      </button>
-      <div className="grid grid-cols-3">
-        <div>
+  const handleCheck = () => {
+    console.log(hasil);
+  };
+
+  return (
+    <div className="w-full h-full border bg-black rounded-lg border-gray-300">
+      <div className="grid grid-cols-3 h-full">
+        <div className=" border p-3  ">
           <h1>Perkalian vektor</h1>
         </div>
-        <div>
-          <h1>=</h1>
+        <div className=" border p-3 ">
+          <h1 className="text-center">=</h1>
         </div>
-        <div>
-          <h1>{hasil && hasil.perkalianVektor}</h1>
+        <div className=" border p-3 ">
+          <h1 >{hasil && hasil.perkalianVektor}</h1>
         </div>
       </div>
       <div className="grid grid-cols-3">
-        <div>
+        <div className=" border p-3 ">
           <h1>Sudut antara 2 vektor</h1>
         </div>
-        <div>
-          <h1>=</h1>
+        <div className=" border p-3 ">
+          <h1 className="text-center">=</h1>
         </div>
-        <div>
-          {hasil ? <h1>{hasil?.sudutVektor}°</h1> : null}
-          
-        </div>
+        <div className=" border p-3 ">{hasil && <h1 >{hasil?.sudutVektor}°</h1>}</div>
       </div>
-      <div className="grid grid-cols-3">
-        <div>
+      <div className="grid grid-cols-3 ">
+        <div className="h-full border-b-0 border p-3 ">
           <h1>Kosinus arah vektor</h1>
         </div>
-        <div>
-          <h1>=</h1>
+        <div className="h-full border-b-0 border p-3 ">
+          <h1 className="text-center">=</h1>
         </div>
-        <div>
+        <div className="h-full  border border-b-0 p-3 ">
           <ul>
             <li>Vektor 1 :</li>
             <li>
               <ul>
-                <li>cos a = { hasil ? hasil.cosinusArah.vektor1.cosa : null}</li>
-                <li>cos b = { hasil ? hasil.cosinusArah?.vektor1.cosb : null}</li>
-                {hasil ? (hasil.cosinusArah?.vektor1.cosc ? (
-                  <li>cos c = {hasil.cosinusArah?.vektor1.cosc}</li>
-                ) : null) : null}
+                <li >
+                  cos a = {hasil && hasil?.cosinusArah.vektor1.cosa}
+                </li>
+                <li >
+                  cos b = {hasil && hasil.cosinusArah?.vektor1.cosb}
+                </li>
+                {hasil &&
+                  (hasil.cosinusArah?.vektor1.cosc ? (
+                    <li >cos c = {hasil.cosinusArah?.vektor1.cosc}</li>
+                  ) : null)}
               </ul>
             </li>
-            <li>Vektor 2 :</li>
+            <li >Vektor 2 :</li>
             <li>
               <ul>
-                <li>cos a = {hasil ? hasil.cosinusArah?.vektor2.cosa : null}</li>
+                <li>cos a = {hasil && hasil.cosinusArah?.vektor2.cosa}</li>
                 <li>cos b = {hasil && hasil.cosinusArah?.vektor2.cosb}</li>
-                {hasil && (hasil.cosinusArah?.vektor2.cosc ? (
-                  <li>cos c = {hasil.cosinusArah?.vektor2.cosc}</li>
-                ) : null)}
+                {hasil &&
+                  (hasil.cosinusArah?.vektor2.cosc ? (
+                    <li>cos c = {hasil.cosinusArah?.vektor2.cosc}</li>
+                  ) : null)}
               </ul>
             </li>
           </ul>
--- app/hitung/page.jsx
+++ app/hitung/page.jsx
@@ -2,7 +2,6 @@
 import { useRef, useState } from "react";
 import HasilHitung from "../components/hasilHitung";
 
-
 const HitungPage = () => {
   const [idList, setIdList] = useState(0);
   const [hasil, setHasil] = useState(null);
@@ -30,10 +29,6 @@ const HitungPage = () => {
       vector1: [],
       vector2: [],
     };
-    if (data.vector1.length !== data.vector2.length) {
-      alert("Jumlah vektor harus sama");
-      return null;
-    }
     vektor1K.current.value === ""
       ? data.vector1.push(vektor1I.current.value, vektor1J.current.value)
       : data.vector1.push(
@@ -48,95 +43,67 @@ const HitungPage = () => {
           vektor2J.current.value,
           vektor2K.current.value
         );
+    if (data.vector1.length !== data.vector2.length) {
+      alert("Jumlah vektor harus sama");
+      return null;
+    }
 
-        setHasil(data)
-
-    data.vector1.length === 3
-      ? setHasil({
-          sudutVektor: angleBetweenVectors(data.vector1, data.vector2),
-          perkalianVektor: dotProduct(data.vector1, data.vector2),
-          cosinusArah: {
-            vektor1: {
-              cosa: kosinusA(data.vector1),
-              cosb: kosinusB(data.vector1),
-              cosc: kosinusC(data.vector1),
-            },
-            vektor2: {
-              cosa: kosinusA(data.vector2),
-              cosb: kosinusB(data.vector2),
-              cosc: kosinusC(data.vector2),
-            },
-          },
-        })
-      : setHasil({
-          sudutVektor: angleBetweenVectors(data.vector1, data.vector2),
-          perkalianVektor: dotProduct(data.vector1, data.vector2),
-          cosinusArah: {
-            vektor1: {
-              cosa: kosinusA(data.vector1),
-              cosb: kosinusB(data.vector1),
-            },
-            vektor2: {
-              cosa: kosinusA(data.vector2),
-              cosb: kosinusB(data.vector2),
-            },
-          },
-        });
-
+    setHasil(data);
+    console.log(hasil);
   };
 
   return (
-    <div className="flex justify-center items-center">
-      <div className="flex flex-col justify-center items-center gap-5">
-        <h1 className="text-3xl font-bold mb-8 text-center">
-          Vector Calculator
-        </h1>
-        <div className="grid grid-cols-3 gap-4 text-center">
-          <div className="items-center justify-center">
-            <button className="btn btn-outline"> Help</button>
+    <div className="flex flex-col justify-center items-center px-10 gap-10 my-10 md:w-full">
+      <h1 className="text-3xl font-bold text-center">Vector Calculator</h1>
+
+      <div className=" flex flex-col md:w-full md:flex-row gap-8 items-stretch justify-center">
+        <div className="flex flex-col gap-8 md:w-[500px] shadow-2xl p-4 bg-gray-900 rounded-lg">
+          <div className="grid grid-cols-3 gap-6 text-center">
+            <div className="items-center justify-center">
+              <button className="btn btn-outline rounded-full"> help</button>
+            </div>
+            <div className="flex items-center justify-center h-full">
+              <h1 className="text-sm">Vektor 1</h1>
+            </div>
+            <div className="flex items-center justify-center h-full">
+              <h2 className="text-sm">Vektor 2</h2>
+            </div>
+            {/* untuk Variabel I */}
+            <div className="text-center">I</div>
+            <div>
+              <input type="number" ref={vektor1I} className="w-10" />
+            </div>
+            <div>
+              <input type="number" ref={vektor2I} className="w-10" />
+            </div>
+            {/* Untuk variabel J */}
+            <div className="text-center">J</div>
+            <div>
+              <input type="number" ref={vektor1J} className="w-10" />
+            </div>
+            <div>
+              <input type="number" ref={vektor2J} className="w-10" />
+            </div>
+            {/* Untuk variabel K */}
+            <div className="text-center">K</div>
+            <div>
+              <input type="number" ref={vektor1K} className="w-10" />
+            </div>
+            <div>
+              <input type="number" ref={vektor2K} className="w-10" />
+            </div>
           </div>
-          <div className="flex items-center justify-center h-full">
-            <h1 className="text-sm">Vektor 1</h1>
-          </div>
-          <div className="flex items-center justify-center h-full">
-            <h2 className="text-sm">Vektor 2</h2>
-          </div>
-          {/* untuk Variabel I */}
-          <div className="text-center">I</div>
-          <div>
-            <input type="number" ref={vektor1I} className="w-10" />
-          </div>
-          <div>
-            <input type="number" ref={vektor2I} className="w-10" />
-          </div>
-          {/* Untuk variabel J */}
-          <div className="text-center">J</div>
-          <div>
-            <input type="number" ref={vektor1J} className="w-10" />
-          </div>
-          <div>
-            <input type="number" ref={vektor2J} className="w-10" />
-          </div>
-          {/* Untuk variabel K */}
-          <div className="text-center">K</div>
-          <div>
-            <input type="number" ref={vektor1K} className="w-10" />
-          </div>
-          <div>
-            <input type="number" ref={vektor2K} className="w-10" />
+          <div className="flex flex-col justify-center">
+            <button
+              onClick={handleAddVector}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Hitung
+            </button>
           </div>
         </div>
-        <div>
-          <button
-            onClick={handleAddVector}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Hitung
-          </button>
-        </div>
+        <HasilHitung vektor1={hasil?.vector1} vektor2={hasil?.vector2} />
       </div>
-      <HasilHitung prop={hasil} />
-      
     </div>
   );
 };
